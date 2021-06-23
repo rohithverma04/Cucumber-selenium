87631cf4ead683c966329c137a0826bd2f2f1c20
Rohithverma <rohithverma.kadari04@gmail.com>
2021-06-23 14:46:28 +0530

    develope

--- src/main/java/pageobjects/flipkartpageobj.java
+++ src/main/java/pageobjects/flipkartpageobj.java
@@ -12,26 +12,8 @@ By search=By.name("q");
 By phone=By.className("_4rR01T");
 By addtocart=By.cssSelector(".ihZ75k");
 
-public flipkartpageobj(WebDriver driver2) {
-	this.driver=driver2;
+public void develope(){
+	System.out.println("develope");
 }
-public WebElement flipkartsearch(){
-	return driver.findElement(search);
-}
-public WebElement flipkartphone(){
-	return driver.findElement(phone);
-}
-public WebElement flipkartaddtocart() throws InterruptedException{
-	Thread.sleep(2000);
-	Set<String> ids=driver.getWindowHandles();
-	Iterator<String> it=ids.iterator();
-	String parent=it.next();
-	String child=it.next();
-	driver.switchTo().window(child);
-	Thread.sleep(2000);
-	//System.out.println(driver.getTitle());
-	return driver.findElement(addtocart);
-}
-
 }
 
